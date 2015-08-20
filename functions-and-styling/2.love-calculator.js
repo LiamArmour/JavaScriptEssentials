a227b11909ab606e605f3ed6231bf9729b1f997d
Liam Armour <liamarmour1991@googlemail.com>
2015-08-20 13:42:31 +0100

    Finished the love calculator

--- functions-and-styling/2.love-calculator.js
+++ functions-and-styling/2.love-calculator.js
@@ -1,3 +1,12 @@
-/**
- * Created by liam.armour on 20/08/15.
- */
+var calculation = function(boyName, girlName) {
+    var foo = (boyName.length + girlName.length)-(Math.random()*6);
+    var result = foo * 42;
+
+    if(result > 100) {
+        console.log(boyName + " loves " + girlName + " 100%")
+    } else {
+        console.log(boyName + " loves " + girlName  +  result + "%")
+    }
+}
+
+calculation("Liam", "Lau");
