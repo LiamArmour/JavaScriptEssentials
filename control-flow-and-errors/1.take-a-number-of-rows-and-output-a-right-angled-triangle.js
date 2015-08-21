79cdf8d4302d9d6e50f92b7d092c3b5790c0c2be
Liam Armour <liamarmour1991@googlemail.com>
2015-08-21 10:55:45 +0100

    finished task one

--- control-flow-and-errors/1.take-a-number-of-rows-and-output-a-right-angled-triangle.js
+++ control-flow-and-errors/1.take-a-number-of-rows-and-output-a-right-angled-triangle.js
@@ -1,3 +1,10 @@
-/**
- * Created by liam.armour on 20/08/15.
- */
+'use strict';
+function makeRightAngle(size) {
+    var stars = "";
+    for(var i = 0 ; i < size ; i++) {
+       stars += "*";
+        console.log(stars);
+    }
+}
+
+makeRightAngle(10);
