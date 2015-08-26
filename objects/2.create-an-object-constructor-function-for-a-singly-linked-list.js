1a35c3daabf9a135511d17894b02167f2e9b64ff
Liam Armour <liamarmour1991@googlemail.com>
2015-08-26 17:24:34 +0100

    Finished task two, it is wrong will fix tomorrow

--- objects/2.create-an-object-constructor-function-for-a-singly-linked-list.js
+++ objects/2.create-an-object-constructor-function-for-a-singly-linked-list.js
@@ -1,47 +1,63 @@
 'use strict';
-var linkedList = function() {
-    var instanceOfLinkedList = object.create(methodsOfLinkedList);
-    instanceOfLinkedList.head = null;
-    instanceOfLinkedList.tail = null;
-    return instanceOfLinkedList;
-};
 
-var methodsOfLinkedList = {
-    add: function(value) {
-        var newNode = makeNode(value);
+(function(){
+    var linkedList = function(){
 
-        if(!this.head) {
-            this.head = newNode;
-        }
-        if(this.tail) {
-            this.tail.next = newNode;
-        }
-        this.tail = newNode;
-        },
-
-    remove: function() {
-        var currentNode = this.head;
-        this.head = currentNode.next;
-        currentNode = null;
-    },
-
-    contains: function(value) {
-        var currentNode = this.head;
-        while (currentNode) {
-            if(currentNode.data === value) {
-                return true;
-            }
-            currentNode = currentNode.next;
-        }
-
-        return false;
-    }
-};
-
-var makeNode = function(value) {
-    var instanceOfNode = {
-        data: value,
-        next: null
+        var instanceOfLinkedList = Object.create(methodsOfLinkedList);
+        instanceOfLinkedList.head = null;
+        instanceOfLinkedList.tail = null;
+        return instanceOfLinkedList;
     };
-    return instanceOfNode;
-};
+
+    var methodsOfLinkedList = {
+        add: function(){
+            var i, j;
+            for (i=0, j=arguments.length; i<j; i++){
+                var newNode = makeNode(arguments[i]);
+                if(!this.head){
+                    this.head = newNode;
+                }
+                if (this.tail){
+                    this.tail.next = newNode;
+                }
+
+                this.tail = newNode;
+            }
+        },
+        remove: function(){
+            var currentNode = this.head;
+            this.head = currentNode.next;
+            currentNode = null;
+        },
+        contains: function(node){
+            var currentNodePosition = this.head;
+            while(currentNodePosition){
+                if (currentNodePosition.data === node){
+                    return true;
+                }
+                currentNodePosition = currentNodePosition.next;
+            }
+            return false;
+        }
+    };
+
+    var makeNode = function(value){
+        var instanceOfNode = {
+            data: value,
+            next: null
+        };
+        return instanceOfNode;
+    };
+
+    var makeLinkedList = linkedList();
+    makeLinkedList.add(1, 2, 3, 4, 5);
+    console.log("Does 1 exist? " + makeLinkedList.contains(1));
+    console.log("Does 2 exist? " + makeLinkedList.contains(2));
+    console.log("-- Now lets remove the first list item");
+    makeLinkedList.remove();
+    console.log("Does 1 exist? " + makeLinkedList.contains(1));
+    makeLinkedList.remove();
+    console.log("-- Now lets remove the next first list item, this should remove 2");
+    console.log("Does 2 exist? " + makeLinkedList.contains(2));
+
+})();
