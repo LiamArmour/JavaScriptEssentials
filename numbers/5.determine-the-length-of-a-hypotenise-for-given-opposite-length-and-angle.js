2d1d11644dddf24d9992331381c76befebbacfc1
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 09:17:37 +0100

    pushing

---
+++ numbers/5.determine-the-length-of-a-hypotenise-for-given-opposite-length-and-angle.js
@@ -0,0 +1,16 @@
+'use strict'
+findHypo(3,4);
+console.log(Hypo);
+
+var hyLength = 0;
+var trigCalc = function(length,angle){
+    hyLength= Math.sin(angle)*length;
+}
+trigCalc(12,30);
+console.log(hyLength);
+
+/*
+ var Hypo=0;
+ var findHypo=function(number1,number2){
+ Hypo=Math.sqrt(((number1*number1)+(number2*number2)));
+ } */
