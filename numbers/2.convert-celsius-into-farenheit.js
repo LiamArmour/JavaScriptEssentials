d15577dfa41fa4d7da8831878ce6372fab63d96b
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 09:18:50 +0100

    pushing live changes

---
+++ numbers/2.convert-celsius-into-farenheit.js
@@ -0,0 +1,7 @@
+'use strict';
+function convert (){
+    var celc = 12;
+    var faren = ((celc * 2) +30);
+    console.log(celc +" degrees celsius equates to: "+faren + " "+ "farenheit");
+}
+convert();
---
+++ numbers/3.convert-degrees-into-radians.js
@@ -0,0 +1,7 @@
+'use strict'
+var convertRad =0;
+var convertDeg=function(number){
+    convertRad=((Math.PI/180)*number);
+}
+convertDeg(345);
+console.log(convertRad);
